c187aee247abb0f0e98c567f3624d8e7310712ac
zainjumuah <your.email@example.com~>
2026-01-27 10:00:52 +0100

    fixed the ridiculous comma bug

--- zeecommerce/settings.py
+++ zeecommerce/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products' #first app
+    'products', #first app
     'users', #second app
     'orders', #third app
     'rest_framework',
