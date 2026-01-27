cabea4c8ce054121f07ccdf7f2beab5013166e7a
zainjumuah <your.email@example.com~>
2026-01-27 10:51:30 +0100

    created product model products/models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255) #
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+    stock = models.PositiveIntegerField(default=0)
+    location = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
