5e438ad2607f922d89aecab66628f670ccf28f49
zainjumuah <your.email@example.com~>
2026-02-07 10:39:54 +0100

    feat(products): register Category router in products/urls.py

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from .views import ProductViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet, basename="product")
+#register categoryviewset
 router.register(r"categories", CategoryViewSet, basename="category")
 
 urlpatterns = [
