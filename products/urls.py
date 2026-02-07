3257a9be25858bacb2e9041a0da02e5050de45ea
zainjumuah <your.email@example.com~>
2026-02-07 10:37:09 +0100

    feat(products): add CategoryViewSet with read-only for anonymous users

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
-from .views import ProductViewSet
+from .views import ProductViewSet, CategoryViewSet
 
 router = DefaultRouter()
 router.register(r"products", ProductViewSet, basename="product")
+router.register(r"categories", CategoryViewSet, basename="category")
 
 urlpatterns = [
     path("", include(router.urls)),
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
-from rest_framework import viewsets
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework import viewsets, permissions
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
@@ -9,3 +9,8 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.select_related("category").all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by("name")
+    serializer_class = CategorySerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
