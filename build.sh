690d933efcc5d2d51472219be45d8cc9840fb6e8
zainjumuah <your.email@example.com~>
2026-02-08 14:20:17 +0100

    ignore this commit guys

--- build.sh
+++ build.sh
@@ -2,6 +2,5 @@
 set -o errexit
 
 pip install -r requirements.txt
-# I force prod settings here so collectstatic/migrate don't accidentally use dev config on Render.
-python manage.py collectstatic --noinput --settings=zeecommerce.settings.prod
-python manage.py migrate --settings=zeecommerce.settings.prod
+python manage.py collectstatic --noinput
+python manage.py migrate
